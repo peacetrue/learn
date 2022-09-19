5354d7ad67d97d290dee18e562006dffe371be2e
peacetrue <xiayouxue@gmail.com>
2022-09-20 07:50:10 +0800

    give up

--- learn-zookeeper/prometheus.sh
+++ learn-zookeeper/prometheus.sh
@@ -2,9 +2,9 @@
 
 #scp root@10.0.0.51:/root/download/prometheus-2.21.0.linux-386/prometheus.yml .
 #scp prometheus.yml root@10.0.0.51:/root/download/prometheus-2.21.0.linux-386
-nodes=($ZOOKEEPER_NODES)
-echo "${nodes[0]  }"
-#for node in ${nodes[@]}; do
-#  echo "node: $node"
-#  ssh "root@$node" "zkServer.sh status"
-#done
+nodes=(`echo $ZOOKEEPER_NODES`)
+for node in ${nodes[@]}; do
+  echo "node: $node"
+  ssh "root@$node" "zkServer.sh start"
+  echo "\n"
+done
